12a0c665913723e7f6102a3c4ba7c847c33b02ff
Rustam161 <rustambabaev161@gmail.com>
2026-08-03 03:55:04 +0300

    Правка 4

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,24 +2,19 @@ import { Montserrat } from "next/font/google";
 import './styles/globals.scss';
 import Header from "./components/ui/Header";
 import Footer from "./components/ui/Footer";
-import { SessionProvider } from "next-auth/react";
-import { auth } from "../../auth";
 
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
-export default async function RootLayout({ children }) {
-  const session = await auth()
+export default function RootLayout({ children }) {
   return (
-    <SessionProvider session={session}>
-      <html lang="en">
-        <body className={montserrat.className}>
-          <Header />
-          <main>{children}</main>
-          <Footer />
-        </body>
-      </html>
-    </SessionProvider>
+    <html lang="en">
+      <body className={montserrat.className}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
 
   );
 }
