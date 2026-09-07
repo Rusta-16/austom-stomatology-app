1a1c60fb8a48c0a50f3b2ce2180b8ea971606b9a
Альбина <albina@MacBook-Air-Albina.local>
2026-09-07 23:37:27 +0300

    Добавленны robot.js и site

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://austom.ru',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
