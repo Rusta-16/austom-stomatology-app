6d8b2896106f105daf6d7d26dea68153c6cfd38e
Альбина <albina@MacBook-Air-Albina.local>
2026-09-09 12:34:56 +0300

    Исправленна ошиька с самозакрывающимся тегом Image

--- src/app/components/StartPage.jsx
+++ src/app/components/StartPage.jsx
@@ -15,23 +15,23 @@ export default function StartPage() {
       </div>
       <div className="infoBlock">
         <div className='imgGirlWithToothbrush'>
-          <Image src='/Galina_doctor.svg' width={100} height={150} alt='Девушка с зубной щёткой' id='girl' priority fetchPriority="high"></Image>
-          <Image src='/rounds.svg' width={200} height={100} alt='фон' id='rounds' priority fetchPriority="high"> </Image>
+          <Image src='/Galina_doctor.svg' width={100} height={150} alt='Девушка с зубной щёткой' id='girl' priority fetchPriority="high"/>
+          <Image src='/rounds.svg' width={200} height={100} alt='фон' id='rounds' priority fetchPriority="high"/>
         </div>
         <div className="blockAdvIcons">
           <div className="advIcons">
             <div className="colRet">
-              <Image src='/Star.svg' width={40} height={40} alt='иконка'></Image>
+              <Image src='/Star.svg' width={40} height={40} alt='иконка'/>
               <p><b>4.9</b></p>
               <p className="iconText">Рейтинг в Яндекс</p>
             </div>
             <div className="colRet">
-              <Image src='/Heart.svg' width={40} height={40} alt='иконка' ></Image>
+              <Image src='/Heart.svg' width={40} height={40} alt='иконка'/>
               <p><b>5000+</b></p>
               <p className="iconText" >Довольных пациентов</p>
             </div>
             <div className="colRet">
-              <Image src='/Sheat.svg' width={40} height={40} alt='иконка' ></Image>
+              <Image src='/Sheat.svg' width={40} height={40} alt='иконка'/>
               <p><b>10 лет</b></p>
               <p className="iconText">Опыта заботы о ваших зубах</p>
             </div>
